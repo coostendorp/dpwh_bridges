0c14a6347de8db40858be6d603c7a263006ed0fc
Caspar Oostendorp <caspar@oostvoort.com>
2022-09-30 11:21:18 +0800

    adding initial clunky osmhelper support, with an initial "generate" script

---
+++ osmhelper/generate.js
@@ -0,0 +1,118 @@
+require("dotenv").config({ path: process.cwd() + "/.env" });
+
+// iD link
+// https://github.com/openstreetmap/iD/blob/develop/API.md
+
+const db = require("osmhelper/db");
+const fs = require("fs-extra");
+
+async function doIt() {
+  await db.query(`
+        CREATE INDEX IF NOT EXISTS dpwh_geom_index ON import.dpwh USING gist ((wkb_geometry::geography));
+        CREATE INDEX IF NOT EXISTS bridge_linestring_geom_index ON import.bridge_linestring USING gist ((geometry::geometry));
+        create unique index IF NOT EXISTS dpwh_bridge_id_uindex on import.dpwh (bridge_id);
+    `);
+
+  const rows = (
+    await db.query(`
+            SELECT d.bridge_id,
+                   d.br_name,
+                   d.road_name,
+                   d.province,
+                   d.br_length,
+                   d.br_width,
+                   d.load_limit,
+                   d.br_type1,
+                   d.actual_yr,
+                    COUNT(h.osm_id) as "osm_count",
+                   json_agg(h.osm_id) as "osm",
+                   ST_AsGeoJSON(
+                           ST_Envelope(
+                                   ST_Buffer(d.wkb_geometry, 0.002)
+                               ))     as "envelope"
+            FROM import.dpwh d
+                     LEFT JOIN import.bridge_linestring h
+                               ON ST_DWithin(d.wkb_geometry, ST_Transform(h.geometry, 4326), 200, false)
+            where d.wkb_geometry is not null
+            GROUP BY d.ogc_fid
+            limit ${process.env["GENERATE_ROWCOUNT"]}
+            ;
+        `)
+  ).rows;
+  let selectcounter = 1;
+  let tsv = [
+    "bridge_id",
+    "name",
+    "road_name",
+    "province",
+    "length",
+    "width",
+    "loadlimit",
+    "type",
+    "actual_yr",
+    "osm_count",
+    "osm_ids",
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+    `josm_${selectcounter++}`,
+  ].join("\t");
+
+  for (const row of rows) {
+    const boxCoords = JSON.parse(row.envelope).coordinates[0];
+
+    const josmSelect = [];
+
+    for (const osm of row.osm) {
+      const url = new URL("http://127.0.0.1:8111/load_and_zoom");
+      url.searchParams.set("left", boxCoords[0][0]);
+      url.searchParams.set("right", boxCoords[2][0]);
+      url.searchParams.set("top", boxCoords[1][1]);
+      url.searchParams.set("bottom", boxCoords[0][1]);
+
+      url.searchParams.set("select", `way${osm}`);
+      if (osm != null) josmSelect.push(url.toString());
+    }
+
+    tsv = tsv
+      .concat("\n")
+      .concat(
+        [
+          row.bridge_id,
+          row.br_name,
+          row.road_name,
+          row.province,
+          row.br_length,
+          row.br_width,
+          row.loadlimit,
+          row.br_type1,
+          row.actual_yr,
+          row.osm_count,
+          `"${row.osm.join("\n")}"`,
+          `${josmSelect.join("\t")}`,
+        ].join("\t")
+      );
+    // console.log(tsv);
+  }
+  await fs.writeFileSync("tmp.tsv", tsv);
+}
+
+async function main() {
+  await doIt();
+}
+
+main();
---
+++ osmhelper/import_dpwh.sh
@@ -0,0 +1,7 @@
+docker exec \
+  osmhelper_postgis_1 \
+  ogr2ogr \
+  -f "PostgreSQL" PG:"port=5444 dbname=postgres user=postgres" "/opt/project/raw_national_bridges_dataset_dpwh-rbi-2022-019.geojson" \
+  -nln \
+  import.dpwh
+
